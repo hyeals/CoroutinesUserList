8d519195412985a0bd44be8d53601de144d18bec
hyeals <hyealsp@gmail.com>
2022-04-13 16:35:54 +0900

    Write Model for Retrofit
    - GithubRepositoryModel.kt 추가
    - RetrofitApi.kt 추가
    - RetrofitClient.kt 추가

---
+++ app/src/main/java/com/example/coroutinestudy/model/RetrofitApi.kt
@@ -0,0 +1,9 @@
+package com.example.coroutinestudy.model
+
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface RetrofitApi {
+    @GET("/users")
+    suspend fun getUsers(): Response<GithubRepositoryModel>
+}
---
+++ app/src/main/java/com/example/coroutinestudy/model/RetrofitClient.kt
@@ -0,0 +1,30 @@
+package com.example.coroutinestudy.model
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+
+    fun getService() = getClient().create(RetrofitApi::class.java)
+
+    private fun getClient(): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(getUrl())
+            .client(getOkHttpClient())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun getUrl(): String{
+        return "http://api.github.com"
+    }
+
+    private fun getOkHttpClient():OkHttpClient{
+        return OkHttpClient().newBuilder()
+            .addNetworkInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }).build()
+    }
+}
